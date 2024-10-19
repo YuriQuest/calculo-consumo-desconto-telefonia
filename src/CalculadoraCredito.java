5339b1112842bf1111db21646f5f9ae34baea8ba
YuriQuest <162909475+YuriQuest@users.noreply.github.com>
2024-10-18 21:49:09 -0300

    ✨Feat: codigo do 1 desafio concluido

--- src/CalculadoraCredito.java
+++ src/CalculadoraCredito.java
@@ -19,6 +19,9 @@ public class CalculadoraCredito {
                 double valorPorMinuto = Double.parseDouble(dadosLigacao[2].trim());
 
                 // TODO: Calcule o custo da ligação
+                if (i < n) {
+                  totalCredito += duracao * valorPorMinuto;
+                }
             }
 
             // Formata o resultado com duas casas decimais e imprime
